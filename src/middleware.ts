750c7091ec75532ad750758f3f827b5ed452cb33
rbxyz <rbcr4z1@gmail.com>
2025-02-25 10:34:23 -0300

    subindo para o vercel corrigindo o middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,6 @@ export default clerkMiddleware();
 export const config = {
   matcher: [
     "/((?!_next|.*\\..*).*)", // 🔹 Evita que o middleware rode em arquivos estáticos
-    "/(api|trpc)(.*)", // 🔹 Sempre roda em rotas API e tRPC
-  ],
+    ["/admin(.*)", "/api(.*)"]
+    ],
 };
