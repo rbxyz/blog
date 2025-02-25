6f79921eaf5f69345d2e7561df4ae117353d6d4a
rbxyz <rbcr4z1@gmail.com>
2025-02-25 10:30:19 -0300

    subindo para o vercel corrigindo o middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,9 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
 
-
 export const config = {
-  runtime: "nodejs",
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/((?!_next|.*\\..*).*)", // 🔹 Evita que o middleware rode em arquivos estáticos
+    "/(api|trpc)(.*)", // 🔹 Sempre roda em rotas API e tRPC
   ],
 };
