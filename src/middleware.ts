792a8e14acc2e48c3831af472441c169c9e0be08
rbxyz <rbcr4z1@gmail.com>
2025-02-26 11:19:59 -0300

    add logs

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,8 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    "/((?!_next|.*\\..*).*)", // 🔹 Evita que o middleware rode em arquivos estáticos
-    "/admin(.*)", // 🔹 Middleware roda para todas as páginas dentro de /admin
-    "/api(.*)", // 🔹 Middleware roda para todas as rotas API
+    "/((?!_next|.*\\..*).*)", 
+    "/admin(.*)", 
+    "/api(.*)", 
   ],
 };
